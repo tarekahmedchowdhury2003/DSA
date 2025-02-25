5029de88c21736dcadce7d6e919a3edc2c530aec
TarekChy <tarekchy569@gmail.com>
2025-02-25 20:42:43 +0600

    initial commit

---
+++ SelectionSort.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+void SelectionSort(int arr[], int n){
+    int min;
+    for(int i=0; i<n-1; i++){
+        min = i;
+    
+        for(int j= i+1; j<n; j++){
+            if(arr[j] >arr[min]){
+                min = j;
+            }
+        }
+
+        if(min != i){
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+    }
+}
+
+int main(){
+    int n;
+    cin>>n;
+
+    int arr[n];
+
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+
+    SelectionSort(arr, n);
+
+    for(int i=0; i<n; i++){
+        cout<<arr[i];
+    }
+
+
+
+}
